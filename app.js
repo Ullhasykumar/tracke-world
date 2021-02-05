335bab8a4b9357b5aa85edf2ec4b505f3992a2a6
Ullhasykumar <69715731+Ullhasykumar@users.noreply.github.com>
2021-02-05 20:26:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,150 @@
+const countries = document.querySelector("datalist");
+const search = document.querySelector("#search");
+const date = document.querySelector("#date");
+const nameCountry = document.querySelector("#name-country");
+const confirmed = document.querySelector(".confirmed");
+const deaths = document.querySelector(".deaths");
+const recovered = document.querySelector(".recovered");
+const chart = document.querySelector(".chart");
+const chart1 = document.querySelector(".chart1");
+
+let dataChart = [];
+
+const API_URL = "https://api.covid19api.com/summary";
+
+async function covid(country) {
+  countries.innerHTML = `<option value="World">World</option>`;
+  resetValue(confirmed);
+  resetValue(deaths);
+  resetValue(recovered);
+  const res = await fetch(API_URL);
+  const data = await res.json();
+
+  if (res.status === 4 || res.status === 200) {
+    date.textContent = new Date(data.Date).toDateString();
+
+    if (country === "" || country === "World") {
+      const {
+        TotalConfirmed,
+        TotalDeaths,
+        TotalRecovered,
+        NewConfirmed,
+        NewDeaths,
+        NewRecovered,
+      } = data.Global;
+      total(TotalConfirmed, TotalDeaths, TotalRecovered);
+      newUpdate(NewConfirmed, NewDeaths, NewRecovered);
+
+      nameCountry.textContent = "Worldwide";
+      dataChart = [TotalConfirmed, TotalDeaths, TotalRecovered];
+    }
+
+    data.Countries.forEach((item) => {
+      const option = document.createElement("option");
+      option.value = item.Country;
+      option.textContent = item.Country;
+      countries.appendChild(option);
+      if (country === item.Country) {
+        total(item.TotalConfirmed, item.TotalDeaths, item.TotalRecovered);
+        newUpdate(item.NewConfirmed, item.NewDeaths, item.NewRecovered);
+
+        nameCountry.textContent = item.Country;
+        dataChart = [
+          item.TotalConfirmed,
+          item.TotalDeaths,
+          item.TotalRecovered,
+        ];
+      }
+    });
+    drawChart(dataChart);
+    drawLineChart(dataChart);
+  } else {
+    chart.innerHTML = `<h2>Loading...<.h2>`;
+  }
+}
+
+function counting(target, element) {
+  const inc = target / speed;
+  const count = +element.textContent;
+  if (count < target) {
+    element.textContent = Math.ceil(count + inc);
+    setTimeout(() => {
+      counting(target, element);
+    }, 20);
+  } else {
+    element.textContent = target;
+  }
+}
+
+const speed = 100;
+
+function total(Confirmed, Deaths, Recovered) {
+  // Total Confirmed
+  counting(Confirmed, confirmed.children[1]);
+
+  // Total Deaths
+  counting(Deaths, deaths.children[1]);
+  // Total Recovered
+  counting(Recovered, recovered.children[1]);
+}
+
+function newUpdate(Confirmed, Deaths, Recovered) {
+  // New  Confirmed
+  counting(Confirmed, confirmed.children[2]);
+  // New  Deaths
+  counting(Deaths, deaths.children[2]);
+  // New Recovered
+  counting(Recovered, recovered.children[2]);
+}
+
+function resetValue(element) {
+  element.children[1].textContent = 0;
+  element.children[2].textContent = 0;
+}
+
+function drawChart(data) {
+  chart.innerHTML = "";
+  const ctx = document.createElement("canvas");
+  chart.appendChild(ctx);
+  const myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Total Confirmed", "Total Deaths", "Total Recovered"],
+      datasets: [
+        {
+          label: nameCountry.textContent,
+          data: data,
+          backgroundColor: ["crimson", "black", "green"],
+        },
+      ],
+    },
+    options: {},
+  });
+}
+function drawLineChart(data) {
+  chart1.innerHTML = "";
+  const ctx = document.createElement("canvas");
+  chart.appendChild(ctx);
+  const myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: ["Total Confirmed", "Total Deaths", "Total Recovered"],
+      datasets: [
+        {
+          label: nameCountry.textContent,
+          data: data,
+          backgroundColor: ["crimson", "black", "green"],
+        },
+      ],
+    },
+    options: {},
+  });
+}
+
+covid(search.value);
+const btnSeacrh = document.querySelector("button");
+btnSeacrh.addEventListener("click", (e) => {
+  e.preventDefault();
+  covid(search.value);
+  search.value = "";
+});
